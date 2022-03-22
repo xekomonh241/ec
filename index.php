6b85cad90546ba1b188b34ab24353a935f27c32a
Parikun1412 <justinbk1412@gmail.com>
2022-03-22 10:23:04 +0700

    1

--- index.php
+++ index.php
@@ -39,6 +39,8 @@ require_once($path . '/class/product.php')
     <!-- Main Style -->
     <link rel="stylesheet" href="assets/css/style.css" />
 
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
 </head>
 
 <body>
@@ -674,6 +676,18 @@ require_once($path . '/class/product.php')
     <!-- JavaScripts -->
     <?php require_once($path . '/../includes/scripts.php') ?>
     <!-- END JavaScripts -->
+
+    <script>
+        function checklogin() {
+            var data = $('#formlogin').serialize();
+            $.ajax({
+                url: <?php echo  ?>
+            })
+        }
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
